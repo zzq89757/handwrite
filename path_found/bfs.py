18a38717af28804590168a71029157faa788c4c2
zzq89757 <1604653835@qq.com>
2024-07-10 14:45:12 +0800

    bfs start

---
+++ path_found/bfs.py
@@ -0,0 +1,9 @@
+import numpy as np
+class BFS:
+    def find_path_by_bfs(self, graph:np.ndarray, start:tuple, end:tuple):
+        pass
+        
+        
+if __name__ == "__main__":
+    bfs = BFS()
+    bfs.find_path_by_bfs()
