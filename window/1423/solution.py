dcdbdb48695c7f5eecc2b06cf6422f8b7027aed0
zzq89757 <1604653835@qq.com>
2024-07-11 09:30:18 +0800

    1423 soluted

--- window/1423/solution.py
+++ window/1423/solution.py
@@ -1,7 +1,7 @@
 class Solution:
     def maxScore(self, cardpoint:list, k:int):
         n = len(cardpoint)
-        total = sum(card_point)
+        total = sum(cardpoint)
         if n == k:return total
         # window n - k
         
@@ -10,13 +10,11 @@ class Solution:
         for  i in range(n - k):
             _sum += cardpoint[i]
         _max_sum = _sum
-        
         # window move
         for i in range(n - k, n):
-            _remove_item = cardpoint[i - (n - k) + 1]
+            _remove_item = cardpoint[i - (n - k)]
             _append_item = cardpoint[i]
             _sum = _sum - _remove_item + _append_item
-            
             if _sum < _max_sum:_max_sum = _sum
         
         return total - _max_sum
@@ -24,8 +22,8 @@ class Solution:
         
         
 if __name__ == "__main__":
-    card_point = [1, 2, 3, 4, 5, 6, 1]
-    k = 3
+    card_point = [96,90,41,82,39,74,64,50,30]
+    k = 8
     s = Solution()
     max_score = s.maxScore(card_point, k)
     print(max_score)
