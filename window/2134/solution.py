a6b8a782413a11343e20fd8216b1e6534eb26114
zzq89757 <1604653835@qq.com>
2024-07-11 22:37:55 +0800

    no clue

--- window/2134/solution.py
+++ window/2134/solution.py
@@ -3,20 +3,22 @@ class Solution:
         # 统计1的个数作为 window size，1最多的窗口中0的数目即为所求
         n = len(num)
         k = sum(num)
-        # 逆向思维：统计1的个数k，n-k 为window size，找和最小的窗口
+        # 逆向思维：统计1的个数k，n-k 为window size，把窗口里的1往外丢 （错误的）应该 应该往外丢0 把1往里丢（前提是外面还有1）（错误的）应该让窗口都是0 但
+        # 
         # 需考虑0比1多(n-k>k)时 能够交换的1 不够的问题！！
         # 第一个窗口
         _min = _current = sum(num[:n - k])
         # 后续窗口 仅考虑滑出和滑入的值
-        for i in range(n - k, n):
-            _current += num[i] - num[i - (n - k)]
+        for i in range(n - k, n): 
+            # 0比1多 那当前窗口1的数目只能交换k-_current次
+            if n > 2 * k:
+                _current = 
+            else:
+                _current += num[i] - num[i - (n - k)]
             if _current < _min:
                 _min = _current
      
         return _min
-
-
-
 
 
 if __name__ == "__main__":
