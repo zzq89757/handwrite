5ac617c7fe57997823c279d1e6e1edf9b763bcbb
zzq89757 <1604653835@qq.com>
2024-07-13 03:42:38 +0800

    to be corrected

--- window/2653/solution.py
+++ window/2653/solution.py
@@ -4,13 +4,29 @@ class Solution:
         # 统计数组长度 window size 为 k
         beauty_list = []
         n = len(num)
-        ct = Counter()
         # 滑动取值
         # 第一个窗口
-        for i in range(k):
-            ct[num[i]] += 1
-            if 
-        print(ct) 
+        # ct = Counter(num[:k])
+        # 判断倒数第x个元素 即正数第 k-x+1 个
+        
+        # 后续窗口
+        for i in range(n -k):
+            if i == 0:ct = Counter(num[:k])
+            current_sum = 0
+            is_add = 0
+            for val, count in ct.items():
+                current_sum += count
+                if val < 0 and current_sum >= k - x + 1:
+                    beauty_list.append(val)
+                    is_add = 1
+                    break
+            if not is_add:beauty_list.append(0)
+            ct[num[i-1]] += 1
+            ct[num[i + k]] -= 1
+            if ct[num[i-1]] == 0:ct.pop(num[i-1])
+            print(ct)
+        
+        print(beauty_list) 
         
         # return _min
 
