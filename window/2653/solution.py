5f38bba15e86e798d68a9915117f45896aaa85ea
zzq89757 <1604653835@qq.com>
2024-07-16 22:52:14 +0800

    2653 in a mess

--- window/2653/solution.py
+++ window/2653/solution.py
@@ -4,16 +4,22 @@ class Solution:
         # 统计数组长度 window size 为 k
         beauty_list = []
         n = len(num)
+        if k == n:
+            _x_val = sorted(num,reverse=False)[x-1]
+            return [_x_val] if _x_val <= 0 else [0]
         # 滑动取值
         # 第一个窗口
         ct = Counter(num[:k])
-        # 判断倒数第x个元素 即正数第 k-x+1 个
+        # 判断倒数第x个元素 即正数第 k - x + 1 个
         
         # 后续窗口
+            
+            
         for i in range(k,n + 1):
             # 逆向操作 发现一个次数减一 移除则加1 然后当k==x时用 most_common x ==1 时 则
-            if k == x:
-                ct
+            # if k == x:
+            #     ct
+            
             current_sum = 0
             is_add = 0
             for val, count in sorted(ct.items(),key=lambda x:x[0], reverse=True):
@@ -32,7 +38,7 @@ class Solution:
 
 
 if __name__ == "__main__":
-    nums = [1,-1,-3,-2,3]
+    # nums = [1,-1,-3,-2,3]
     # nums = [-1,-2,-3,-4,-5]
     nums = [-50,14]
     # nums = [1,1,1,0,0,1,0,1,1,0]
---
+++ window/2653/solution_1.py
@@ -0,0 +1,20 @@
+from collections import Counter
+class Solution:
+    def getSubarrayBeauty(self, num: list[int], k: int, x: int) -> list[int]:
+      	
+
+
+
+
+
+if __name__ == "__main__":
+    nums = [1,-1,-3,-2,3]
+    # nums = [-1,-2,-3,-4,-5]
+    nums = [-50,14]
+    # nums = [1,1,1,0,0,1,0,1,1,0]
+    s = Solution()
+    # k = 3
+    k = 2
+    x = 2
+    res = s.getSubarrayBeauty(nums, k, x)
+    print(res)
